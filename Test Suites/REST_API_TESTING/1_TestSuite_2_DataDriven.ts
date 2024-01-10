2c843323844aad943cab5b9b3608403e60c42e6a
jgorbiso <jgorbiso@gmail.com>
2024-01-10 16:36:22 +0800

    This is another test

--- Test Suites/REST_API_TESTING/1_TestSuite_2_DataDriven.ts
+++ Test Suites/REST_API_TESTING/1_TestSuite_2_DataDriven.ts
@@ -4,7 +4,7 @@
    <name>1_TestSuite_2_DataDriven</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <mailRecipient></mailRecipient>
+   <mailRecipient>kat_01@mailinator.com;</mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
